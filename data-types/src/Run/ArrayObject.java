32bbb1317b34bfea779154d9f72103c35d7dc7a3
EnoshTs <enosh.tsur@haaretz.co.il>
2019-11-08 11:53:47 +0200

    fixed turn to tern

--- data-types/src/Run/ArrayObject.java
+++ data-types/src/Run/ArrayObject.java
@@ -3,70 +3,57 @@ package Run;
 import java.util.Arrays;
 
 public class ArrayObject {
-	
+
 	static void printArrays(int[] nums) {
 		System.out.println(Arrays.toString(nums));
 	}
-	
-	
+
 	public static void main(String[] args) {
 		// Declare array
 		int[] intArray = new int[4];
-		
+
 		// Initial array
-		for(int i = 0; i < intArray.length; i++) {
+		for (int i = 0; i < intArray.length; i++) {
 			int randomNumber = (int) (Math.random() * 10);
 			intArray[i] = randomNumber;
 		}
-		
+
 		// Sum array
 		int sum = 0;
 		for (int current : intArray) {
-			sum+= current;
+			sum += current;
 		}
-		
+
 		int max = 0;
 		int counter = 0;
 		int higherIndex = 0;
-		
+
 		for (int current : intArray) {
-			if(current > max) {
+			if (current > max) {
 				max = current;
 				higherIndex = counter;
 			}
-			
+
 			counter++;
 		}
-		
-		System.out.printf("the max: %d a index %d", max, higherIndex);
-		
-		
-		
-		int[] intArray2 = {
-				(int) (Math.random() * 10),
-				(int) (Math.random() * 10),
-				(int) (Math.random() * 10),
-				(int) (Math.random() * 10)
-		};
-		
 
-		
+		System.out.printf("the max: %d a index %d", max, higherIndex);
+
+		int[] intArray2 = { (int) (Math.random() * 10), (int) (Math.random() * 10), (int) (Math.random() * 10),
+				(int) (Math.random() * 10) };
+
 		printArrays(intArray);
 		int counter2 = 0;
-		for(int current: intArray) {
+		for (int current : intArray) {
 			int random = (int) (Math.random() * 10);
 			intArray[counter2] = random;
 			counter2++;
 		}
-	
-		
+
 		printArrays(intArray);
 
-		
-	System.out.println(Arrays.toString(intArray));
-		
-		
-		
+		System.out.println(Arrays.toString(intArray));
+
 	}
 
 }
